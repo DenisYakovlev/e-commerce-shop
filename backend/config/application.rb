f90ba6f3660bdee5e1f181a27c398fdcf29d3971
DenisYakovlev <denis.yakovlev098@gmail.com>
2024-02-20 13:01:27 +0200

    added env file and cors

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -2,6 +2,9 @@ require_relative "boot"
 
 require "rails/all"
 
+require 'dotenv'
+Dotenv.load('.env')
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -28,6 +31,8 @@ module Backend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.server_port = ENV['PORT'].to_i if ENV['PORT']
 
     # devise session store fix
     config.session_store :cookie_store, key: '_interslice_session'
