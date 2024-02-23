d9314e4aaaf7cd01822165ca592badfafe0b3d9b
DenisYakovlev <denis.yakovlev098@gmail.com>
2024-02-23 20:26:56 +0200

    changing configs to make Dockerfile

--- backend/app/helpers/JwtHelper.rb
+++ backend/app/helpers/JwtHelper.rb
@@ -1,9 +1,10 @@
 require 'jwt'
 
 module JwtHelper
-  @@secret_key = Rails.application.credentials.jwt[:secret_key]
+  @@secret_key = ENV['JWT_SECRET']
 
   def encode_token(payload)
+    puts @@secret_key
     JWT.encode(payload, @@secret_key)
   end
 
